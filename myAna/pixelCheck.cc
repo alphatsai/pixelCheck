a19f5f360197916db4a0c748378b6cb3781f1a5e
Alpha <jui-fa.tsai@cern.ch>
2013-12-12 19:30:06 +0100

    Update TH1InfoClass.h with template

--- myAna/pixelCheck.cc
+++ myAna/pixelCheck.cc
@@ -8,8 +8,11 @@
 #include "TFile.h"
 #include "TH1D.h"
 #include "../interface/format.h"
-#include "../interface/TH1InfoClass.h"
+#include "../interface/TH1Info.h"	// Histogram details are defined here
+#include "../interface/TH1InfoClass.h"	 
 using namespace std;
+
+typedef TH1D TH1_Type;
 
 const string samplePath    = "/afs/cern.ch/work/r/rslu/public/FNAL2013_data";
 const string storeRootPath = "../result/root";
@@ -37,10 +40,10 @@ void pixelCheck(){
 
 	for( int isample=0; isample<Sample_Size; isample++){
 		////= Input and prepare the out files ===========================================================================
-		string input  	=    samplePath + "/" + sampleName[isample] + ".root";
-		string output 	= storeRootPath + "/" + sampleName[isample] + ".root";
-		TFile* input_f  = new TFile(input.c_str()); 
-		TFile* output_f = new TFile(output.c_str(),"RECREATE"); 
+		string input 	=    samplePath + "/" + sampleName[isample] + ".root";
+		string output	= storeRootPath + "/" + sampleName[isample] + ".root";
+		TFile* input_f	= new TFile(input.c_str()); 
+		TFile* output_f	= new TFile(output.c_str(),"RECREATE"); 
 		cout<<"Success read "<<input<<" !"<<endl;	
 	
 		////= Regist tree and branch ====================================================================================
@@ -50,6 +53,15 @@ void pixelCheck(){
 		cout<<"Success register tree!"<<endl;	
 
 		////= Create Histogram and sub-directory ========================================================================
+		TH1InfoClass<TH1_Type> h[ROC_Size];
+		for( int index=0; index<ROC_Size; index++){ 
+			output_f->mkdir(index_ROC[index]);
+			output_f->cd(index_ROC[index]);
+				h[index].Initialize();
+				cout<<"Success create TH1 in "<<index_ROC[index]<<" !"<<endl;	
+			output_f->cd(); 
+		}
+	
 		TH1D* h_hits = new TH1D("TotalHits", "Total Hits", 9, -1, 8);
 		h_hits->GetXaxis()->SetBinLabel(1,"X_X");
 		h_hits->GetXaxis()->SetBinLabel(2,index_ROC[0]);
@@ -59,32 +71,23 @@ void pixelCheck(){
 		h_hits->GetXaxis()->SetBinLabel(6,index_ROC[4]);
 		h_hits->GetXaxis()->SetBinLabel(7,index_ROC[5]);
 		h_hits->GetXaxis()->SetBinLabel(8,index_ROC[6]);
-		h_hits->GetXaxis()->SetBinLabel(9,index_ROC[7]);
+		h_hits->GetXaxis()->SetBinLabel(9,index_ROC[7]);	
 
-		TH1InfoClass<TH1D> h[ROC_Size];
-		for( int index=0; index<ROC_Size; index++){ 
-			output_f->mkdir(index_ROC[index]);
-			output_f->cd(index_ROC[index]);
-				h[index].Initialize();
-				cout<<"Success create TH1 in "<<index_ROC[index]<<" !"<<endl;	
-			output_f->cd(); 
-		}	
-		
 		////= Loop for each hit =========================================================================================
 		cout<<"Running..."<<endl;	
 		for( int hit=0; hit<tree->GetEntries(); hit++){
 			tree->GetEntry(hit);			
 			h_hits->Fill(Hit.ROCnumber);
-			if( Hit.ROCnumber < 0 ) continue; //Some Hit.ROCnumber = -1	
-			output_f->cd(index_ROC[Hit.ROCnumber]);// Enter the ROC_number directory
+			if( Hit.ROCnumber < 0 ) continue; 	// Some Hit.ROCnumber = -1	
+			output_f->cd(index_ROC[Hit.ROCnumber]);	// Enter the ROC_number directory
 
 				h[Hit.ROCnumber].GetTH1("ROCnumber")->Fill(Hit.ROCnumber);
 
-			output_f->cd();// Exit
-		}//Hit
+			output_f->cd(); // Exit
+		} // Hit
 		output_f->Write();
 		cout<<"Success write into "<<output<<" !"<<endl<<endl;	
-	}//sample
+	} // sample
 }
 
 
