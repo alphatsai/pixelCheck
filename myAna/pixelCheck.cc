ae30efdc04a034372f3f8ae98cbfd616a257e6dd
Alpha <jui-fa.tsai@cern.ch>
2013-12-13 01:33:27 +0100

    Create script

--- myAna/pixelCheck.cc
+++ myAna/pixelCheck.cc
@@ -15,6 +15,22 @@
 #include "../interface/TH2InfoClass.h"	 
 using namespace std;
 
+// Detail information for data
+const int Row_Size = 80;
+const int Col_Size = 52;
+const int ROC_Size = 8;
+const char* index_ROC[ROC_Size] = {
+	"ROC_0",
+	"ROC_1",
+	"ROC_2",
+	"ROC_3",
+	"ROC_4",
+	"ROC_5",
+	"ROC_6",
+	"ROC_7",
+};
+
+// Change int to string
 string int2str(int &i){
 	string s;
 	stringstream ss(s);
